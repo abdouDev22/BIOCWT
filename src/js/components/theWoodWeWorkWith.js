2b94decf90be5f96fbf7e879193e20094b6e07ad
abdouDev22 <abdillahiabdourahman65@gmail.com>
2024-11-17 12:00:25 +0300

    ajout de nouveau element

--- src/js/components/theWoodWeWorkWith.js
+++ src/js/components/theWoodWeWorkWith.js
@@ -6,10 +6,39 @@ export function theWoodWeWorkWith (){
     title.classList.add("title");
     title.textContent = "THE WOOD WE WORK WITH";
 
-    div.appendChild(createImageElement("./src/img/old-wood-grain-background 2.jpg"));
-    div.appendChild(createImageElement("./src/img/wood-texture-design-decoration 1.jpg"));
-    div.appendChild(createImageElement("./src/img/pale-oak-wood-texture-design-background 1.jpg"));
+
     div.appendChild(title);
+    div.appendChild(createContent("Oak",
+        ["Durability", "Beautiful texture", "Water resistance", "Expensive"],
+        "./src/img/old-wood-grain-background 2.jpg",["h", "h", "h", "c"]));
+
+
     return div
 }
 import { createImageElement } from '../components/createImageElement.js'
+
+
+function createContent(title, listItems, imagePath, types) {
+    const content = document.createElement("div");
+    content.classList.add("content");
+
+    const titleh4 = document.createElement("h4");
+    titleh4.classList.add("titleh4");
+    titleh4.textContent = title;
+
+    const ul = document.createElement("ul");
+    listItems.forEach((itemText, index) => {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        span.classList.add(types[index]); // Use the type from the array at current index
+        li.appendChild(span);
+        li.appendChild(document.createTextNode(itemText));
+        ul.appendChild(li);
+    });
+
+    content.appendChild(createImageElement(imagePath));
+    content.appendChild(titleh4);
+    content.appendChild(ul);
+
+    return content;
+}
